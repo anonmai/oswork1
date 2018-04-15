0f018e01675ea5cf06d5528de9df8649b2eb74d0
Ouyang <4509767770@qq.com>
2018-04-15 14:13:26 +0800

    180415

---
+++ bootloader/start.s
@@ -0,0 +1,34 @@
+.code16
+
+.global start
+start:
+        cli                             #关闭中断
+        inb $0x92, %al                  #启动A20总线
+        orb $0x02, %al
+        outb %al, $0x92
+        data32 addr32 lgdt gdtDesc      #加载GDTR
+        movl %cr0, %eax                 #启动保护模式
+        orb $0x01, %al
+        movl %eax, %cr0
+        data32 ljmp $0x08, $start32
+.code32
+start32:
+		jmp bootMain
+
+.p2align 2
+gdt:
+	.word 0,0 # empty entry
+	.byte 0,0,0,0
+
+	.word oxffff,0
+	.byte 0,0x9a,0xcf,0
+
+	.word 0xffff,0
+	.byte 0,0x92,0xcf,0
+
+	.word 0xffff,0x8000
+	.byte 0x0b,0x92,0xcf,0
+
+gdtDesc:
+	.word (gdtDesc - gdt -1)
+	.long gdt
